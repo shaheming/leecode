8d69ca000988236829bdf9b2aec6c277ae5b98f3
sand <shmsand@gmail.com>
2018-07-16 23:11:47 +0800

    Update

--- 2018_07_15/reverse-linked-list-ii.py
+++ 2018_07_15/reverse-linked-list-ii.py
@@ -43,6 +43,34 @@ class Solution:
         tmpHead.next = tmpPre.next
 
         return emptyHead.next
+    #这里的核心思想是用了一个指针来存储 reverse
+    def reverseBetween2(self, head, m, n):
+        if m == n:
+            return head
+
+        dummyNode = ListNode(0)
+        dummyNode.next = head
+        pre = dummyNode
+
+        for i in range(m - 1):
+            pre = pre.next
+
+        # reverse the [m, n] nodes
+        reverse = None
+        cur = pre.next
+        for i in range(n - m + 1):
+            next = cur.next
+            cur.next = reverse
+            print_list(cur)
+            reverse = cur
+            print("reverse:")
+            print_list(reverse)
+            cur = next
+        print_list(cur)
+        pre.next.next = cur
+        pre.next = reverse
+
+        return dummyNode.next
 
 
 def print_list(listNode):
@@ -63,4 +91,6 @@ for i in data[1:]:
 
 print_list(head)
 s = Solution()
-print_list(s.reverseBetween(head, 1, 4))
+# print_list(s.reverseBetween(head, 1, 4))
+
+s.reverseBetween2(head, 1, 4)
